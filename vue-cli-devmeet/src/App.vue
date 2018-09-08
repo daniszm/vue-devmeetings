586cb9db8ba0817e7ed0ca7d17ee8ea7294bd719
daniszm <damymyda@Daniszs-MacBook-Pro.local>
2018-09-08 13:37:25 +0200

    [#3.2] Create a component for a single list item

--- vue-cli-devmeet/src/App.vue
+++ vue-cli-devmeet/src/App.vue
@@ -1,36 +1,21 @@
 <template>
   <div id="app">
-   <!-- 1. Here we have some content -->
-    <h1>Vue.js</h1>
-
-    <p>is ready to start an adventure</p>
-
-    <!-- 6. If element matches the selector we added, Vue will create a binding -->
-    <div id="app">
         <!-- 7. We can use Angular-like double brackets to create an expression -->
-        <div v-for="elem in items" :key="elem.id">
-            <p style="display: inline-block">{{ elem.name }}</p> 
-            <button style="display: inline-block" @click="removeElem(elem.id)">X</button>
-        </div>
-        <form @submit.prevent="onSubmit()">
-          <!-- <button @click="addElem()">Add</button> -->
-          <label>Your:</label>
-          <input type="text" name="productName" v-model="newProduct.name" v-validate="'required|min:4'">
-          <div v-show="errors.has('productName')">
-            {{errors.first('productName')}}
-          </div>
-
-          <button>Add</button>
-        </form>
-    </div>  
-  </div>  
+        <ProductList :items="items" @test="removeElem"></ProductList>
+        <FormComponent @submit-form="onSubmit"></FormComponent>
+    </div>   
 </template>
 
 <script>
-import uuid from 'uuid/v4';
+import ProductList from './components/ProductList'
+import FormComponent from './components/FormComponent'
 
 export default {
   name: "app",
+  components: {
+    ProductList,
+    FormComponent
+  },
   data() {
     return {
       items: [
@@ -46,27 +31,13 @@ export default {
           id: 2,
           name: "Nazwa 3"
         }
-      ],
-      newProduct: {
-        name: ''
-      }
+      ]
     };
   },
   methods: {
-    onSubmit() {
-      this.$validator.validateAll().then(result => {
-        if (!result) {
-          return;
-        }
-        this.items.push({
-          id: uuid(),
-          ...this.newProduct
-        });
-        this.newProduct.name = "";
-        this.validator.reset();
-    })
+    onSubmit(obj) {
+        this.items.push(obj);
     },
-
     addElem() {
       let currentId = this.items[this.items.length - 1].id;
       let newId = currentId + 1;
@@ -74,13 +45,12 @@ export default {
         id: newId,
         name: this.inputText
       };
-      console.log(inputObj);
       this.items.push(inputObj);
       this.inputText = "";
     },
 
     removeElem(id) {
-      this.items = this.items.filter(obj => obj.id != id);
+        this.items = this.items.filter(obj => obj.id != id);
     }
   }
 };
